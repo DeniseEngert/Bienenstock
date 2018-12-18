8a569da278911f4b417bc6f19be49269cdb9cd6e
Philipp Gieseler <contact@philippgieseler.com>
2018-12-18 16:47:29 +0100

    set media folder

--- databee/settings.py
+++ databee/settings.py
@@ -129,5 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
+
+# MEDIA_ROOT (for debugging/testing)
+MEDIA_ROOT = 'media/'
+MEDIA_URL = '/media/'
