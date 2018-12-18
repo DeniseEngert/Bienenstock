66291e95ba5c807ac7ee2a3a3a2e69aee391b13f
Philipp Gieseler <contact@philippgieseler.com>
2018-12-18 16:07:02 +0100

    add profiles app

---
+++ profiles/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    areas_of_interest = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=12)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_profile')
