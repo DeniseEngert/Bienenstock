4c9da52792cc6a1a8e0b3c672d73a7adb90f33ce
Philipp Gieseler <contact@philippgieseler.com>
2019-01-02 13:08:38 +0100

    add new fields to page model

--- pages/models.py
+++ pages/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # model for basic pages
 class Page(models.Model):
+    title = models.CharField(max_length=30, blank=False, default="")
+    image = models.ImageField(upload_to='images', null=True)
+    teaser = models.CharField(max_length=100, blank=True)
     text = models.TextField(max_length=4000)
