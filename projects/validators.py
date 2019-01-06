08aaa726b97f7eb69a48ca7c8c50756735989cb3
Philipp Gieseler <contact@philippgieseler.com>
2019-01-06 19:10:40 +0100

    try to parse csv file in dataset file validator

--- projects/validators.py
+++ projects/validators.py
@@ -3,7 +3,18 @@ import os
 import csv
 
 def validate_csv(value):
+    # check if extension is correct
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
     valid_extensions = ['.csv']
     if not ext.lower() in valid_extensions:
         raise ValidationError(u'Unsupported file extension.')
+
+    # try to parse csv file
+    try:
+        str_text = ''
+        for line in value.file:
+            str_text = str_text + line.decode()
+        csv.reader(value.file)
+    except:
+        raise ValidationError(u'Wrong format.')
+
