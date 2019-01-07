3882232ad15b14d0756669938ece08d8e34942c7
deniseengert <engertden@gmail.com>
2019-01-07 20:08:06 +0100

    dynamic card-view, extended project-model

--- projects/forms.py
+++ projects/forms.py
@@ -5,7 +5,7 @@ from .models import *
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ('title', 'description', 'is_public')
+        fields = ('title', 'description', 'category', 'is_public')
 
 
 class DatasetForm(ModelForm):
