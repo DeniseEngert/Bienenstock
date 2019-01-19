d9f8c98a48a47016a421abefbcf5ad505fdf478d
Philipp Gieseler <contact@philippgieseler.com>
2019-01-19 13:21:23 +0100

    reset migrations

--- profiles/forms.py
+++ profiles/forms.py
@@ -18,4 +18,4 @@ class UserForm(ModelForm):
 class ProfileView(ModelForm):
     class Meta:
         model = Profile
-        fields = ('areas_of_interest', 'address', 'phone_number', 'birth_date', 'bio')
+        fields = ('areas_of_interest', 'address', 'phone_number', 'birth_date', 'bio', 'image')
