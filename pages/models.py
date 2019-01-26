460a205ad0c9bd6bb6e7f98d5e763b574aea184c
Philipp Gieseler <contact@philippgieseler.com>
2019-01-26 15:38:33 +0100

    fix pages and settings model for easier editign in admin panel, change/add settings fields

--- pages/models.py
+++ pages/models.py
@@ -2,20 +2,20 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 from django.template.defaultfilters import slugify
 from markupfield.fields import MarkupField
+from django.utils.translation import gettext_lazy as _
 
 
 class Page(MPTTModel):
-    title = models.CharField(max_length=30, blank=False, default="")
-    image = models.ImageField(upload_to='images', blank=True, null=True)
-    teaser = models.CharField(max_length=100, blank=True)
-    text = MarkupField(markup_type='markdown', escape_html=True)
-    slug = models.SlugField(max_length=60, blank=False)
+    title = models.CharField(_('title'), max_length=30, blank=False, default="")
+    image = models.ImageField(_('image'), upload_to='images', blank=True, null=True)
+    teaser = models.CharField(_('teaser'), max_length=100, blank=True)
+    text = MarkupField(_('text'), markup_type='markdown', escape_html=True)
+    slug = models.SlugField(_('slug'), max_length=60, blank=True, unique=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
-    # generate slug when saving
     def save(self, *args, **kwargs):
-        if not self.id:
-            self.slug = slugify(self.title)  # Or whatever you want the slug to use
+        if not self.id or self.slug == '':
+            self.slug = slugify(self.title)
         super(Page, self).save(*args, **kwargs)
 
     class MPTTMeta:
