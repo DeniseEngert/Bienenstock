ceda5a39c7397baf82205016b10a0d68e40b1d07
Philipp Gieseler <contact@philippgieseler.com>
2019-01-27 12:05:15 +0100

    use fields in templates depending on language

--- common/context_processors.py
+++ common/context_processors.py
@@ -2,3 +2,6 @@ from .models import SiteSettings
 
 def settings(request):
     return {'settings': SiteSettings.load()}
+
+def lang_context_processor(request):
+    return {'lang': request.LANGUAGE_CODE}
--- databee/settings.py
+++ databee/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'common.context_processors.settings',
+                'common.context_processors.lang_context_processor',
                 'pages.context_processors.settings',
             ],
         },
